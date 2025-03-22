5f4f12ef87d8412d8a0a948ddc2adc5c9e82c320
Kyazs <santosjohncasper@gmail.com>
2025-03-23 00:26:21 +0800

    fixed bugs on routes

--- public/index.php
+++ public/index.php
@@ -13,6 +13,13 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+// electron
+if (isset($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'Electron') !== false) {
+    $baseUrl = 'file://' . __DIR__;
+} else {
+    $baseUrl = isset($_SERVER['HTTP_HOST']) ? (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] : 'http://localhost';
+}
+
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
